77300f44d87e506aa65409416b9a9fd032e42e72
Andreas Riegg <andreas.riegg@gmx.de>
2022-01-16 15:31:30 +0100

    AppImage

--- package.sh
+++ package.sh
@@ -20,7 +20,13 @@ mkdir -p $PKGDIR
 cp -r $LIBDIR/* $PKGDIR
 
 if [ $TARGET = "linux" ]; then
-	cp $ROOTDIR/$GAME $PKGDIR 
+    cp -r $LIBDIR/squashfs-root $PKGDIR/squashfs-root
+	cd $PKGDIR
+	cat squashfs-root/bin/love $ROOTDIR/$GAME > squashfs-root/bin/$NAME
+    chmod +x squashfs-root/bin/$NAME
+    sed -i "s/NAME/$NAME/" squashfs-root/love.desktop
+    appimagetool squashfs-root Elysion.AppImage
+    rm -r squashfs-root
 elif [ $TARGET = "windows" ]; then
 	cd $PKGDIR
 	cat $LIBDIR/love.exe $ROOTDIR/$GAME > $NAME.exe
